0a80afc114f83f7e3907c44f59b2d95b7c22ea88
Batmanisc0d1ng <136151106+Batmanisc0d1ng@users.noreply.github.com>
2023-08-27 21:33:32 +0530

    Add files via upload

---
+++ BasicCalculator.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Basic Calculator");
+        System.out.println("Available operations: +, -, *, /");
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result = 0.0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator.");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+
+        scanner.close();
+    }
+}
